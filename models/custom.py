27e6e3bfb6c8bab025cce81eb749b6e83504e4dd
Duy Hoàng <duypo03cbvn@gmail.com>
2025-06-12 16:53:27 +0700

    remove tolist

--- models/custom.py
+++ models/custom.py
@@ -4,7 +4,7 @@ class CustomDataset(torch.utils.data.Dataset):
     def __init__(self, input_data, targets, transform=None):
         self.data = [input_data[i].unsqueeze(0).float() for i in range(input_data.size(0))]
         self.targets = targets
-        self.classes = torch.unique(targets).tolist()
+        self.classes = torch.unique(targets)
 
     def __len__(self):
         return len(self.data)
