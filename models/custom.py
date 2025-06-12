80d61ab40e253084721e9b2b7bb14047a597f7d8
Duy Hoàng <duypo03cbvn@gmail.com>
2025-06-12 16:59:37 +0700

    fix custom dataset to run on list

--- models/custom.py
+++ models/custom.py
@@ -2,9 +2,10 @@ import torch
 
 class CustomDataset(torch.utils.data.Dataset):
     def __init__(self, input_data, targets, transform=None):
-        self.data = [input_data[i].unsqueeze(0).float() for i in range(input_data.size(0))]
+        self.data = input_data
         self.targets = targets
-        self.classes = torch.unique(targets)
+        self.classes = torch.unique(torch.tensor(targets)).tolist()
+
 
     def __len__(self):
         return len(self.data)
