Fix formula in feddc

algorithm/feddc.py
@@ -31,26 +31,25 @@ class FedDC(FedAvg):
         results: List[Tuple[ClientProxy, FitRes]],
         failures: List[Union[Tuple[ClientProxy, FitRes], BaseException]]) -> Tuple[Optional[Parameters], Dict[str, Scalar]]:
 
-        weights = []
-        total_samples = 0
         drifts = []
         delta_gs = []
         total_examples = sum(fit_res.num_examples for _, fit_res in results)
+        weights_results = [(parameters_to_ndarrays(fit_res.parameters), fit_res.num_examples) for _, fit_res in results]
 
         for client, fit_res in results:
-            arr = parameters_to_ndarrays(fit_res.parameters)
-            weight = fit_res.num_examples
-            weights.append((arr, weight))
-            total_samples += weight
+           
             drifts.append(
                 np.frombuffer(fit_res.metrics['drift'], dtype=np.float64))
             delta_gs.append(
                 np.frombuffer(fit_res.metrics['delta_g'], dtype=np.float64))
 
-        aggregated = aggregate(weights)
-        self.current_parameters = ndarrays_to_parameters(aggregated)
+        aggregated = aggregate(weights_results)
+        avg_drifts = np.sum(drifts, axis=0) / len(drifts)
+        tuned_aggregated = aggregated + avg_drifts
+    
+        self.current_parameters = ndarrays_to_parameters(tuned_aggregated)
         delta_g_mean = np.sum(delta_gs, axis=0) / len(delta_gs)
-        self.state_grad_diff += delta_g_mean
+        self.state_grad_diff = delta_g_mean.copy()
 
         metrics_aggregated = {}
 